84fca5de05d3329a3ed081c1f72304c71e44d190
Pannkaj <pnkghogare27@gmail.com>
2024-12-16 17:05:20 +0530

    updated List

--- src/Components/LoginList.jsx
+++ src/Components/LoginList.jsx
@@ -8,7 +8,7 @@ const LoginList = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/users");
+        const response = await axios.get("http://localhost:3000/User");
         setUsers(response.data);
       } catch (error) {
         console.error("Error fetching user data", error);
