84635cf02fa77ed41a6af3a67b9a2a248e139ce6
Pannkaj <pnkghogare27@gmail.com>
2024-12-16 17:47:24 +0530

    updated List

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
-import RegistrationForm from './Components/RegistrationForm'
-import {Route, Routers} from 'react-router-dom'
-
-
-
+import RegistrationForm from "./Components/RegistrationForm";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import LoginPage from "./Components/Login_Page";
-import Navbar from './Components/Navbar';
-import LoginList from './Components/LoginList';
-
+import Navbar from "./Components/Navbar";
+import LoginList from "./Components/LoginList";
 
 const App = () => {
   return (
-    <div>
-
-<Routers>
-<Route path="/" elements={<LoginPage/>}/>
-<Route path="/Registration" elements={<RegistrationForm/>}/>
-<Route path="/dashboard" elements={<LoginList/>}/>
-</Routers>
-
-    
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/Registration" element={<RegistrationForm />} /> 
+        <Route path="/dashboard" element={<LoginList />} /> 
+      </Routes>
+    </Router>
   );
 };
 
 export default App;
+
